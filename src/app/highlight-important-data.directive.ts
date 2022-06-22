fd926d067de4edfdad147578ad33d437e0a7b730
Sahithi <w0767287@myscc.ca>
2022-06-22 18:21:27 +0530

    border added

--- src/app/highlight-important-data.directive.ts
+++ src/app/highlight-important-data.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
 
-  constructor() { }
+  constructor(public elementRef:ElementRef) { }
+  @Input('appHighlightImportantData') hoverClass:any;  
+  
+  @HostListener('mouseenter') onMouseEnter() {
+    this.elementRef.nativeElement.classList.add(this.hoverClass); //using hostlistener adding the functioanlity of mouseneter
+ }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.elementRef.nativeElement.classList.remove(this.hoverClass);
+  }
+
 
 }
