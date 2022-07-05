f3913c133122b82e8928894a3789356b7aa47a93
Sahithi <w0767287@myscc.ca>
2022-07-05 18:49:03 +0530

    search added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from '../content.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  card;
+  err = false;
+  constructor(public content:ContentService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getData(id){
+    this.content.getContentOnIdbasis(id).subscribe(res =>{
+      console.log(res, 'res');
+      if (res.id== -1) {
+        this.err = true;
+      }
+      this.card = res;
+    })
   }
 
 }
