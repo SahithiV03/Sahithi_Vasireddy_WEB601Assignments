eab6328c1e9930e3311e0978541db7d708db5130
Sahithi <w0767287@myscc.ca>
2022-07-05 18:56:09 +0530

    detail assignment added

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ContentService } from '../content.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,16 +8,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-
-  constructor(private _Activatedroute:ActivatedRoute){
+  content;
+  constructor(private _Activatedroute:ActivatedRoute, public _content:ContentService){
  
     
     const id=this._Activatedroute.snapshot.paramMap.get("id");
     console.log(id, 'd');
+    this.getData(id);
   }
 
 
   ngOnInit(): void {
   }
 
+  getData(id){
+    this._content.getContentOnIdbasis(id).subscribe(res =>{
+      console.log(res, 'res');
+      this.content = res;
+    })
+  }
 }
